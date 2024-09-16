e92349af56f4f722e2d306e6d14f1d8e4dcfed15
Ashna Pradhan <ashnapradhan28@gmail.com>
2024-09-15 23:56:45 -0700

    Update game.cpp

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1,9 +1,9 @@
 #include "../header/game.hpp"
 #include "../header/player.hpp"
+#include "../header/helperFunctions.hpp"
+#include "../header/prompter.hpp"
 
-#include <random>
 #include <stdexcept>
-#include <time.h>
 
 using std::cin;
 using std::cout;
@@ -13,29 +13,33 @@ using std::string;
 
 Game::Game() {}
 
+Game::~Game() {
+    delete player;
+}
+
 void Game::intro() {
     string name;
     int classNumber;
 
-    printLargerSpacer();
+    helperPrintLargerSpacer();
     cout << "Hello adventurer, what is thy name?" << endl;
 
     if (!(cin >> name)) {
-        throw runtime_error("intro read name");
+        throw runtime_error("game intro cin_name");
     }
-    printLargerSpacer();
+    helperPrintLargerSpacer();
 
-    cout << "Greeting " << name << "choose your class. [1-3]" << endl;
-    cout <<  "1. Swordsman" << endl;
+    cout << "Greeting " << name << " choose your class. [1-3]" << endl;
+    cout << "1. Swordsman" << endl;
     cout << "2. Mage" << endl;
     cout << "3. Archer" << endl;
 
     if (!(cin >> classNumber)) {
-        throw runtime_error("intro read class");
+        throw runtime_error("game intro cin_classNumber");
     }
-    printLargerSpacer();
+    helperPrintLargerSpacer();
 
-    Player(classNumber, name);
+    player = new Player(classNumber, name);
 
     cout << "Good luck, " << name << " the ";
     if (classNumber == 1) {
@@ -48,100 +52,58 @@ void Game::intro() {
         cout << "Archer." << endl;
     }
 
-    wait(3);
-    printLargerSpacer();
- }
+    helperWait(2);
+    helperPrintLargerSpacer();
+}
 
- void Game::outro() {
-    printLargerSpacer();
-    cout << "Congratulations on beating the boss! You have saved this land" << endl;
+void Game::outro() {
+    helperPrintLargerSpacer();
+    cout << "Congrats on completing your adventure! You will be remembers for your feats!" << endl;
+}
 
- }
-
- int Game::getDisanceFromFinalBoss() {
+int Game::getDisanceFromFinalBoss() {
     return distanceFromFinalBoss;
- }
+}
 
- void Game::travelCloserToFinalBoss() {
-    if (distanceFromFinalBoss == 1) {
-        distanceFromFinalBoss--;
+void Game::travelCloserToFinalBoss(int travelDistance) {
+    if(distanceFromFinalBoss - travelDistance <= 0) {
+        distanceFromFinalBoss = 0;
+        helperPrintLargerSpacer();
+        cout << "You traveled " << travelDistance << " miles. You have reached the Demon King's Castle!" << endl;
+        helperWait(2.5);
+    } else {
+        distanceFromFinalBoss -= travelDistance;
+        cout << "You traveled " << travelDistance << " miles. The Demon King is " << distanceFromFinalBoss << " miles away!" << endl;
+        helperWait(2.5);
+        helperPrintLargerSpacer();
     }
-    else {
-        srand(time(NULL));
-        distanceFromFinalBoss -= ((rand() % 2) + 1);
+}
+
+void Game::startLethalEncounter() {
+    if (!(encounterPrompter.runFightEncounter(distanceFromFinalBoss, *player))) {
+        isAlive = false;
+        return;
+    } 
+    travelCloserToFinalBoss(4);
+}
+
+void Game::startNonlethalEncounter() {
+    int encounterSelector = helperGenerateRandomInteger(0, 99);
+
+    if (encounterSelector >= 0 && encounterSelector < 50) {
+        encounterPrompter.runHealthEncounter(*player);
+        travelCloserToFinalBoss(1);
+    } else {
+        encounterPrompter.runEquipmentEncounter(*player);
+        travelCloserToFinalBoss(1);
     }
- }
+}
 
- void Game::startEncounter() {
+void Game::startFinalBoss() {
+    cout << "You've reached the Demon King's Castle." << endl;
+    cout << "However, he isn't home! Come back next update, " << player->getName()  << "!" << endl;
+}
 
-    // create a vector of string with 10 or so encounter options to randomly output after the first intro encounter
-
- }
-
- void Game::randomOutcome() {
-    srand(time(NULL));
-
-    int temp = rand() % 100 + 1;
-
-    if (temp <= 30 ) {
-        //int healthInc = getHP() + (rand() % 5 + 1);
-        //setHP(healthInc);
-        //cout << "Congrats you healed " << healthInc << " hp!" << endl;
-    }
-    else if (temp > 30 && temp <= 60) { // random weapon drop
-        string weapon; 
-        cout << "Congrats you've received a new weapon: " << endl;
-        // show stats
-        cout << "Would you like to equip?" << endl;
-        cout << "Enter 1. for yes" << "\n" << " 2. for no" << endl;
-
-        int answer;
-        if (!(cin >> answer)) {
-              throw runtime_error("weapon equip");
-        }
-
-        if (answer == 1) {
-            // equip weapon
-            //setMaxAtk(getMaxAtk() + weapon attack);
-        }
-
-    }
-    else if (temp > 60 && temp <= 90) { // random armor drop
-        string armor;
-        cout << "Congrats you've recieved a new armor: " << endl;
-        // show stats
-        cout << "Would you like to equip?" << endl;
-        cout << "Enter 1. for yes" << "\n" << " 2. for no" << endl;
-
-        int answer;
-        if (!(cin >> answer)) {
-              throw runtime_error("armor equip");
-        }
-
-        if (answer == 1) {
-            // equip armor
-            //seMAxtHP(getMaxHP() + ar attack);
-        }
-    }
-    else if (temp > 90 && temp <= 97) {
-        // pot drop (health pot, temp attack increase, etc)
-        int ranPotDrop = rand() % 2 + 1;
-        if (ranPotDrop == 1) {
-            cout << "You've received a health potion (heals 5 health points)" << endl;
-        }
-        else {
-            cout << "You've received an attack potion (increases attack by 5 for 2 turns)" << endl;
-        }
-    }
-    else {
-        // one revive potion
-        cout << "You've received a revive potion! Use wisely" << endl;
-    }
- }
-
- void Game::startFinalBoss() {
-    cout << "You've reached the final boss." << endl;
-    cout << "Good luck " << /*name << */ endl;
-    // 
- }
-        
+bool Game::isPlayerAlive() {
+    return isAlive;
+}
