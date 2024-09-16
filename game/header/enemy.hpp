8793339918595aa25b04e0fefab0b4ccda1bf68b
Ashna Pradhan <ashnapradhan28@gmail.com>
2024-09-15 23:29:36 -0700

    Update enemy.hpp

--- game/header/enemy.hpp
+++ game/header/enemy.hpp
@@ -2,34 +2,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//#include "character.hpp"
 
 using namespace std;
 
 class Enemy {
  private:
-    vector<string> asciiFiles;
-    int hp, atk, difficulty;
+    int hp, maxHP, atk;
     string name;
-    string defaultMove1Name;
-    string defaultMove2Name;
+    string move1Name;
+    string move2Name;
     
  public:
-    Enemy();
-    Enemy(const string& name, int hp, int atk, int difficulty);
+    Enemy(const string& name, const string& move1Name, const string& move2Name, int hp, int atk);
+
     const string& getName() const;
-    //void getEnemyArt();
+    const string& getMove1Name();
+    const string& getMove2Name();
     int getMaxHP();
     int getHP();
     int getATK();
+
     void setHP(int n);
     void setATK(int n);
     void setName(string n);
-    const string& getMove1Name();
-    const string& getMove2Name();
-    // move1
-    // move2 
-    
-    int getDifficulty();
-    
 };
